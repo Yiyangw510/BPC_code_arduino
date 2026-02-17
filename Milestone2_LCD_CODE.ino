54ff3d7a76a76a86484bd4d63613d404bf62602c
Yiyangw510 <yiyangwww510@gmail.com>
2026-02-17 01:26:39 -0500

    Add files via upload
    Basic function of LCD

---
+++ Milestone2_LCD_CODE.ino
@@ -0,0 +1,56 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); //detect mapping address
+
+enum State{
+    idle,
+    inflate,
+    deflate,
+    hold,
+    emergency,
+    done
+};
+State state = idle;
+
+int pressure = 0;
+
+//convert state to string
+const char* statePrint(State s){
+  switch(s){
+    case idle: return "IDLE";
+    case inflate: return "INFLATE";
+    case hold: return "HOLD";
+    case deflate: return "DEFLATE";
+    case emergency: return "EMERGENCY!!!"
+  }
+}
+
+void setup(){
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+
+void display(int d, State s){
+  lcd.setCursor(0,0);
+  lcd.print("Pressure = ");
+  lcd.print(d);
+  lcd.print(" mmgh");
+
+  lcd.setCursor(0,1);
+  lcd.print("State: ");
+  lcd.print(statePrint(s));
+}
+
+void loop(){
+  int p = readPressure();
+    lcd.setCursor(0,0);
+  lcd.print("Pressure = ");
+  lcd.print(d);
+  lcd.print(" mmgh");
+
+  lcd.setCursor(0,1);
+  lcd.print("State: ");
+  lcd.print(statePrint(s));
+}
